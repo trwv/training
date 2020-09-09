42b26c175a83659ddc9b42dd7b611e067b4ba60e
trwv <thrwav@qq.com>
2020-09-09 15:10:28 +0800

    建立状态机

--- pygame/ant.py
+++ pygame/ant.py
@@ -6,8 +6,7 @@ NEST_SIZE = 100.
 import pygame
 from pygame.locals import *
 from random import randint, choice
-from vector import Vec2d
-
+from vector2d import Vec2d
 
 class GameEntity(object):
     def __init__(self, world, name, image):
@@ -25,12 +24,39 @@ class GameEntity(object):
         surface.blit(self.image, (x-w/2, y-h/2))
     def process(self, time_passed):
         self.brain,think()
-        if self.speed > 0 and slef.location != self.destination:
+        if self.speed > 0 and self.location != self.destination:
             vec_to_destination = self.destination - self.location
             distance_to_destination =vec_to_destination.get_length()
             heading = vec_to_destination.normalized()
             travel_distance = min(distance_to_destination, time_passed * self.speed)
-            self.location += travel_distance += heading
+            self.location += travel_distance * heading
+
+class Ant(GameEntity):
+    def __init__(self, world, image):
+        GameEntity.__init__(self, world, 'ant', image)
+        exploring_state = AntStateExploring(self)
+        seeking_state = AntStateSeeking(self)
+        delivered_state = AntStateDelivered(self)
+        hunting_state = AntStateHunting(self)
+        self.brain.add_state(exploring_state)
+        self.brain.add_state(seeking_state)
+        self.brain.add_state(delivered_state)
+        self.brain.add_state(hunting_state)
+        self.carry_image = None
+    def carry(self, image):
+        self.carry_image = image
+    def drop(self, surface):
+        if self.carry_image:
+            x, y = self.location
+            w, h = self.carry_image.get_size()
+            surface.blit(self.carry_image, (x-w, y-h/2))
+            self.carry_image = None
+    def render(self, surface):
+        GameEntity.render(self, surface)
+        if self.carry_image:
+            x, y = self.location
+            w, h = self.carry_image.get_size()
+            surface.blit(self.carry_image, (x-w, y-h/2))
 
 class World(object):
     def __init__(self):
@@ -66,3 +92,34 @@ class World(object):
                 if distance < range:
                     return entity
         return None
+
+class State():
+    def __init__(self, name):
+        self.name = name
+    def do_actions(self):
+        pass
+    def check_conditions(self):
+        pass
+    def entry_actions(self):
+        pass
+    def exit_actions(self):
+        pass
+
+class StateMachine():
+    def __init__(self):
+        self.states = {}
+        self.active_state = None
+    def add_state(self, state):
+        self.states[state.name] = state
+    def think(self):
+        if self.active_state is None:
+            return
+        self.active_state.do_actions()
+        new_state_name = self.active_state.check_conditions()
+        if new_state_name is not None:
+            self.set_state(new_state_name)
+    def set_state(self, new_state_name):
+        if self.active_state is not None:
+            self.active_state.exit_actions()
+        self.active_state = self.states[new_state_name]
+        self.active_state.entry_actions()
